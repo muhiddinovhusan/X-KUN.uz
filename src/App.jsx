f10091542f0bcc51b2d2ce3fb7adcbc33ab9dea6
Muhiddinov <muhiddinovhusan0207@gmail.com>
2024-03-15 03:02:31 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import News from "./components/News"
 import NewsDetails from "./components/NewsDetails"
+import Home from "./components/Home"
 
 const App = () => {
   return (
     <BrowserRouter>
     <Header/>
-   <Routes>
+   <Routes >
+    <Route path="/" element={<Home/>}/>
     <Route path="/news" element={<News/>}/>
     <Route path="/news/:id" element={<NewsDetails/>}/>
     
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+<h1 className="text-center">Asosiy menyuga otish uchun home page ni tanlang !</h1>
+
+    </div>
+  )
+}
+
+export default Home
